27ca91c8d4dc1cdbd9fb9a20faed56550f83b7bf
Crowdbotics <team@crowdbotics.com>
2021-08-03 14:35:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aline_mobile_0803_d_22843Identifier",
+  url: "https://aline_mobile_0803_d_22843Identifier.botics.co",
+  api: "https://aline_mobile_0803_d_22843Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
